7f4e2e06bc88b13989515bd799201ce15e6b25bc
Kolya Venturi <me@kolya.co>
2021-10-04 08:08:19 -0700

    Add header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import {AppProps} from 'next/dist/shared/lib/router/router';
 import 'tailwindcss/tailwind.css';
+import {Header} from '../components/header';
 
-const App = ({Component, pageProps}: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
-};
+const App = ({Component, pageProps, router}: AppProps): JSX.Element => (
+  <>
+    <Header route={router.asPath} />
+    <Component {...pageProps} />
+  </>
+);
 
 export default App;
