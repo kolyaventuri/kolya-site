1fc5062e82109c4f444d038e39796ecf8571be2c
Kolya Venturi <me@kolya.co>
2021-10-04 07:21:56 -0700

    Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ class Home extends React.Component {
     return (
       <div>
         <Head>
-          <title>Amplify App</title>
+          <title>Kolya Venturi</title>
         </Head>
         <section className="section">
           <h1>Amplify App</h1>
