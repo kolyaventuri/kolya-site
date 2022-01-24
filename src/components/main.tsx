78a7da34d5ceeefb5e2b69c419c70b860d3dbd57
Kolya Venturi <me@kolya.co>
2022-01-23 18:30:42 -0700

    Footer

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 
 import Heading from './heading';
+import Footer from './footer';
 
 const Main = (): JSX.Element => {
   return (
     <div className="h-full w-full top-0 left-0">
       <Heading />
+      <Footer />
     </div>
   );
 };
