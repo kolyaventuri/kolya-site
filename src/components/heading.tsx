af1a1e57286ebc8e9a915ad4bf9f4a9fdb533ee4
Kolya Venturi <kolya.venturi@enx3s.com>
2022-01-24 12:54:17 -0700

    Heading tweaks

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -26,22 +26,24 @@ const Heading = (): JSX.Element => {
     >
       <div className="flex flex-grow flex-col lg:flex-row">
         <div className="w-full relative p-4 m-0">
-          <div className="select-none">
-            <h1
-              className="font-extrabold text-7xl lg:text-very-large p-0 m-0"
-              onMouseOver={() => {
-                toggleUnicorn(true);
-              }}
-              onMouseOut={() => {
-                toggleUnicorn(false);
-              }}
-            >
-              hi there{unicorn ? <Unicorn /> : '.'}
-            </h1>
-            <p className="font-bold text-5xl p-0 m-0 ml-2">
-              i&apos;m <span className="text-green-500">kolya</span>
-            </p>
-            <div className="w-3/4 p-8 pr-4 text-xl">
+          <div>
+            <span className="select-none">
+              <h1
+                className="font-extrabold text-7xl lg:text-very-large p-0 m-0"
+                onMouseOver={() => {
+                  toggleUnicorn(true);
+                }}
+                onMouseOut={() => {
+                  toggleUnicorn(false);
+                }}
+              >
+                hi there{unicorn ? <Unicorn /> : '.'}
+              </h1>
+              <p className="font-bold text-5xl p-0 m-0 ml-1">
+                i&apos;m <span className="text-green-500">kolya</span>
+              </p>
+            </span>
+            <div className="w-3/4 p-8 pr-4 pl-0 lg:pl-8 text-xl">
               <ul className="list-none leading-7">
                 <li className="py-2">
                   <p>
@@ -61,24 +63,24 @@ const Heading = (): JSX.Element => {
                 <li className="py-2">
                   <p>
                     i focus on the frontend in{' '}
-                    <span className="text-blue-500 font-semibold">React</span>{' '}
+                    <span className="font-semibold">React</span>{' '}
                     and{' '}
-                    <span className="text-blue-500 font-semibold">
+                    <span className="font-semibold">
                       TypeScript
                     </span>
                     , and{' '}
-                    <span className="text-red-500 font-semibold">AWS</span> on
+                    <span className="font-semibold">AWS</span> on
                     the backend.
                   </p>
                 </li>
                 <li className="py-2">
                   <p>
                     i&apos;m a self-taught{' '}
-                    <span className="text-pink-500 font-semibold">
+                    <span className="font-semibold">
                       lighting designer
                     </span>{' '}
                     and{' '}
-                    <span className="text-yellow-800 font-semibold">
+                    <span className="font-semibold">
                       coffee enthusiast
                     </span>
                     .
@@ -88,7 +90,7 @@ const Heading = (): JSX.Element => {
                   <p>
                     you can reach me on Twitter{' '}
                     <a
-                      className="text-blue-500 font-semibold"
+                      className="text-blue-500 font-semibold underline"
                       href="https://twitter.com/kolyaventuri"
                       target="_blank"
                       rel="noreferrer"
@@ -97,7 +99,7 @@ const Heading = (): JSX.Element => {
                     </a>{' '}
                     or drop me a line at{' '}
                     <a
-                      className="text-blue-500 font-semibold"
+                      className="text-blue-500 font-semibold underline"
                       href="mailto:me@kolya.co"
                       target="_blank"
                       rel="noreferrer"
